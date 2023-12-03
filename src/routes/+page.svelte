4ae8ba52a34a69b180f27822d1bd79aca1275d60
Garik Asplund <garik.asplund@gmail.com>
2023-12-03 12:34:31 -0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center -translate-y-12">
+		<h2 class="h2 md:text-6xl text-3xl text-secondary-100">APCV 2024 SG</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<img src="SG.png">
+		</figure>
+		<div class="space-y-6">
+			<p class="font-bold md:text-4xl text-2xl text-secondary-50">July 10-12</p>
+			<p><code class="code md:text-xl text-lg">National University of Singapore</code></p>
+			<p>More information soon</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	figure {
+		@apply flex relative flex-col;
+	}
+	figure svg,
+	.img-bg {
+		@apply w-64 h-64 md:w-80 md:h-80;
+	}
+	.img-bg {
+		@apply absolute z-[-1] rounded-full blur-[100px] transition-all;
+		animation: pulse 8s cubic-bezier(0, 0, 0, 0.5) infinite,
+			glow 8s linear infinite;
+	}
+	@keyframes glow {
+		0% {
+			@apply bg-primary-400/40;
+		}
+		33% {
+			@apply bg-secondary-400/40;
+		}
+		66% {
+			@apply bg-tertiary-400/40;
+		}
+		100% {
+			@apply bg-primary-400/40;
+		}
+	}
+	@keyframes pulse {
+		50% {
+			transform: scale(2.5);
+		}
+	}
+</style>

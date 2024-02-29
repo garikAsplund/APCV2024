2d4b5eefd6331de3bddbbdd7b9be1dbf2adbad02
Garik Asplund <garik.asplund@gmail.com>
2024-02-28 21:41:10 -0800

    adding abstract form

---
+++ src/routes/abstract/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	let talk: string;
+</script>
+
+<div class="container h-full mx-auto flex justify-center">
+	<div class="space-y-10 text-center flex flex-col items-center mt-12">
+		<h2
+			class="md:text-6xl lg:text-8xl decoration-0 text-3xl text-secondary-100-token leading-loose tracking-wider"
+		>
+			Abstract submission form
+		</h2>
+
+		<div class="space-y-6">
+			<label class="label">
+				<span>Name</span>
+				<input class="input" title="First name" type="text" placeholder="First" />
+				<input class="input" title="Last name" type="text" placeholder="Last" />
+			</label>
+			<label class="label">
+				<span>Email</span>
+				<input
+					class="input"
+					title="Email"
+					type="email"
+					placeholder="john@example.com"
+					autocomplete="email"
+				/>
+			</label>
+
+			{#each ['talk', 'poster'] as talkType}
+			<button
+				class="chip {talk === talkType ? 'variant-filled' : 'variant-soft'}"
+				on:click={() => { talk = talkType; }}
+				on:keypress
+			>
+				{#if talk === talkType}<span><i class="fa-solid fa-check" /></span>{/if}
+				<span>{talkType}</span>
+			</button>
+			{/each}
+
+			<label class="label">
+				<span>Title</span>
+				<input class="input" title="Title" type="text" placeholder="Title" />
+			</label>
+			<label class="label">
+				<span>Title</span>
+				<input class="input" title="Title" type="text" placeholder="Title" />
+			</label>
+			<label class="label">
+				<span>Abstract</span>
+				<input class="input" type="file" />
+			</label>
+		</div>
+	</div>
+</div>

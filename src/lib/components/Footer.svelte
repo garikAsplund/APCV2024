9f51abac425f698085afa60b7df170532c1267c4
Garik Asplund <garik.asplund@gmail.com>
2025-02-20 08:26:10 -0800

    fixed footer--remove email

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,9 @@
 <div class="flex justify-center mt-24 mb-8 opacity-30">
-	<div class=" scale-75 sm:scale-100 flex justify-evenly md:w-1/2 lg:w-2/5 translate-x-9">
+	<div class=" scale-75 sm:scale-100 flex w-full justify-center">
 		<a href="https://sites.google.com/view/apcv-portal" target="_blank">APCV Portal</a>
-		<span class="mx-2">|</span>
-        <p>apcv2024@gmail.com</p>
-		<span class="mx-2">|</span>
+		<!-- <span class="mx-2">|</span>
+        <p>apcv2024@gmail.com</p> -->
+		<span class="mx-4">|</span>
 		<a href="/privacy">Privacy policy</a>
 	</div>
 </div>
